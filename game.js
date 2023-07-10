804d8b357bdffaa662c82926938c572c14584160
MikaelBhai <126885689+mikaelbhai@users.noreply.github.com>
2023-07-10 11:48:20 +0500

    Update game.js

--- game.js
+++ game.js
@@ -1,16 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
   const gameBoard = document.getElementById('game-board');
+  const scoreElement = document.getElementById('score');
+  
+  const mazeLayout = [
+    // Maze layout goes here, represented by a 2D array
+  ];
 
-  const pacman = document.createElement('div');
-  pacman.classList.add('pacman');
-  gameBoard.appendChild(pacman);
+  const mazeWidth = mazeLayout[0].length;
+  const mazeHeight = mazeLayout.length;
 
   const cellSize = 24;
-  const gameBoardWidth = gameBoard.offsetWidth;
-  const gameBoardHeight = gameBoard.offsetHeight;
+  const pacmanRadius = 10;
 
   let pacmanX = 0;
   let pacmanY = 0;
+  let score = 0;
+
+  function createMaze() {
+    for (let row = 0; row < mazeHeight; row++) {
+      for (let col = 0; col < mazeWidth; col++) {
+        const cell = document.createElement('div');
+        cell.classList.add('maze');
+        cell.style.top = `${row * cellSize}px`;
+        cell.style.left = `${col * cellSize}px`;
+
+        if (mazeLayout[row][col] === 0) {
+          const food = document.createElement('div');
+          food.classList.add('food');
+          cell.appendChild(food);
+        }
+
+        gameBoard.appendChild(cell);
+      }
+    }
+  }
+
+  function createPacman() {
+    const pacman = document.createElement('div');
+    pacman.classList.add('pacman');
+    gameBoard.appendChild(pacman);
+    return pacman;
+  }
 
   function updatePacmanPosition() {
     pacman.style.transform = `translate(${pacmanX * cellSize}px, ${pacmanY * cellSize}px)`;
@@ -18,20 +48,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function handleKeyPress(event) {
     const key = event.key.toLowerCase();
-    const pacmanSpeed = 1;
+
+    let newPacmanX = pacmanX;
+    let newPacmanY = pacmanY;
 
     if (key === 'w' && pacmanY > 0) {
-      pacmanY -= pacmanSpeed;
+      newPacmanY -= 1;
     } else if (key === 'a' && pacmanX > 0) {
-      pacmanX -= pacmanSpeed;
-    } else if (key === 's' && pacmanY < gameBoardHeight / cellSize - 1) {
-      pacmanY += pacmanSpeed;
-    } else if (key === 'd' && pacmanX < gameBoardWidth / cellSize - 1) {
-      pacmanX += pacmanSpeed;
+      newPacmanX -= 1;
+    } else if (key === 's' && pacmanY < mazeHeight - 1) {
+      newPacmanY += 1;
+    } else if (key === 'd' && pacmanX < mazeWidth - 1) {
+      newPacmanX += 1;
     }
 
-    updatePacmanPosition();
+    if (mazeLayout[newPacmanY][newPacmanX] !== 1) {
+      pacmanX = newPacmanX;
+      pacmanY = newPacmanY;
+      updatePacmanPosition();
+    }
+
+    checkCollision();
   }
 
+  function checkCollision() {
+    const food = document.querySelector('.maze:nth-child(' + (pacmanY * mazeWidth + pacmanX + 1) + ') .food');
+
+    if (food) {
+      food.remove();
+      score++;
+      scoreElement.textContent = `Score: ${score}`;
+    }
+  }
+
+  createMaze();
+  const pacman = createPacman();
   document.addEventListener('keydown', handleKeyPress);
 });
