0f85443f7a072d34da9ea2f7b27cfc92171a93e3
MikaelBhai <126885689+mikaelbhai@users.noreply.github.com>
2023-07-10 11:34:24 +0500

    Update and rename script.js to game.js

---
+++ game.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const gameBoard = document.getElementById('game-board');
+  const mazeLayout = [
+    // Maze layout goes here, represented by a 2D array
+  ];
+
+  let score = 0;
+
+  function createMaze() {
+    for (let row = 0; row < mazeLayout.length; row++) {
+      for (let col = 0; col < mazeLayout[row].length; col++) {
+        const cell = document.createElement('div');
+        cell.classList.add('maze');
+        cell.style.top = `${row * 24}px`;
+        cell.style.left = `${col * 24}px`;
+        if (mazeLayout[row][col] === 0) {
+          const food = document.createElement('div');
+          food.classList.add('food');
+          cell.appendChild(food);
+        }
+        gameBoard.appendChild(cell);
+      }
+    }
+  }
+
+  function createPacman() {
+    const pacman = document.createElement('div');
+    pacman.classList.add('pacman');
+    gameBoard.appendChild(pacman);
+    return pacman;
+  }
+
+  function createGhost() {
+    const ghost = document.createElement('div');
+    ghost.classList.add('ghost');
+    gameBoard.appendChild(ghost);
+    return ghost;
+  }
+
+  function handleKeyPress(event) {
+    // Handle arrow key presses to move Pac-Man
+    // Implement movement logic here
+  }
+
+  function checkCollision() {
+    // Check for collision between Pac-Man and ghosts or food
+    // Implement collision detection logic here
+  }
+
+  function updateScore() {
+    const scoreElement = document.getElementById('score');
+    scoreElement.textContent = `Score: ${score}`;
+  }
+
+  function updateGame() {
+    // Update the game state
+    // Implement game logic here
+  }
+
+  function gameLoop() {
+    updateGame();
+    checkCollision();
+    updateScore();
+    requestAnimationFrame(gameLoop);
+  }
+
+  createMaze();
+  const pacman = createPacman();
+  const ghost = createGhost();
+  document.addEventListener('keydown', handleKeyPress);
+  gameLoop();
+});
